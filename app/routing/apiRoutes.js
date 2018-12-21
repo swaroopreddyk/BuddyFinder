d3ffaaad54912497988ff7436cc6262c9fa34ca9
Swaroop Kondreddy <swaroopreddy.k@gmail.com>
2018-12-21 03:25:09 -0500

    add friends details

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,35 @@ module.exports = function (app) {
   app.get("/friends", (req, res) => {
     res.send(friends);
   })
+
+  app.post("/friends", (req, res) => {
+    const profile = {
+      "name"     : req.body.name,
+      "answers"  : req.body.answers.map(a => parseInt(a))
+  };
+
+  const friend = findBestFriend(profile);
+
+  res.send({
+    "my_name"         : profile.name,
+    "friend_name"     : friend.name,
+    "friend_photo_url": friend.photo_url
+});
+  })
+
+  const findBestFriend = profile => {
+    friends.sort((a, b) => findDifference(a, profile) - findDifference(b, profile));
+
+        return friends[0];
+  }
+
+  const findDifference = (a, b) =>{
+    let score = 0;
+
+    for (let i = 0; i < a.answers.length; i++) {
+        score += Math.abs(b.answers[i] - a.answers[i]);
+    }
+
+    return score;
+}
 }
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 7500;
 
+// Parse request body as JSON
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
